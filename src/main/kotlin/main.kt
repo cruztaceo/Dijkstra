7a82f0211e9c0fac46cf279cf5fa193da5871c44
Alejandro Cruz <alejandro.cruz@outlook.com>
2021-06-19 00:19:27 -0700

    Print results

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -9,6 +9,10 @@ fun main() {
     val jsonGraph = Json.decodeFromString<JsonGraph>(json)
 
     println(jsonGraph.toString())
+
+    val pair = dijkstra(jsonGraph, 0, 14)
+    println(pair.first)
+    println(pair.second.contentToString())
 }
 
 /**
@@ -18,3 +22,32 @@ fun main() {
  * @return List of strings read
  */
 fun readFileAsLinesUsingBufferedReader(fileName: String): String = File(fileName).bufferedReader().use { it.readText() }
+
+fun dijkstra(graph: JsonGraph, source: Int, target: Int): Pair<MutableMap<Int, Int>, Array<Int?>> {
+    val Q = graph.nodes.map { it }.toMutableList()
+
+    val dist = graph.nodes.associateWith { Int.MAX_VALUE }.toMutableMap()
+    val prev = arrayOfNulls<Int?>(graph.nodes.size)
+
+    dist[source] = 0
+
+    while (Q.isNotEmpty()) {
+        val u = dist.filter { Q.contains(it.key) }.minByOrNull { it.value }?.key!!
+
+        Q.remove(u)
+
+        if (u == target)
+            return Pair(dist, prev)
+
+        graph.edges.filter { edge -> edge[0] == u && Q.contains(edge[1]) }
+            .forEach { neighbor ->
+                val alt = dist[u]?.plus(neighbor[2])
+                if (alt!! < dist[neighbor[1]]!!) {
+                    dist[neighbor[1]] = alt
+                    prev[neighbor[1]] = u
+                }
+            }
+
+    }
+    return Pair(dist, prev)
+}
--- src/main/kotlin/models/JsonGraph.kt
+++ src/main/kotlin/models/JsonGraph.kt
@@ -3,22 +3,22 @@ package models
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class JsonGraph(val nodes: IntArray, val edges: Array<Int>) {
+data class JsonGraph(val nodes: Set<Int>, val edges: Array<IntArray>) {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
 
         other as JsonGraph
 
-        if (!nodes.contentEquals(other.nodes)) return false
-        if (!edges.contentEquals(other.edges)) return false
+        if (nodes != other.nodes) return false
+        if (!edges.contentDeepEquals(other.edges)) return false
 
         return true
     }
 
     override fun hashCode(): Int {
-        var result = nodes.contentHashCode()
-        result = 31 * result + edges.contentHashCode()
+        var result = nodes.hashCode()
+        result = 31 * result + edges.contentDeepHashCode()
         return result
     }
 
